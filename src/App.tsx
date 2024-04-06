2ee4fa282eab4c2c16a766bc2f001c03144ac6bc
chiya22 <tomoyay@gmail.com>
2024-04-06 13:24:15 +0900

    ストレージへの保存処理を残し終了

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,48 @@
 import { useState } from 'react';
 
 import { TodoList } from './components/TodoList';
+import { AddTodoForm } from './components/AddTodoForm';
+import { TodoSummary } from './components/TodoSummary';
 
 import { dummyTodoList } from './data/dummyTodoList';
 
 function App() {
   const [todoList, setTodoList] = useState(dummyTodoList);
+
+  // Todoを追加
+  const addTodo = (title: string) => {
+    setTodoList((prevTodoList) => {
+      // 新しいTodoを作成
+      const newTodo = {
+        id: Date.now(),
+        title,
+        completed: false,
+      };
+
+      // 変更前のTodoリストと合わせる
+      return [newTodo, ...prevTodoList];
+    });
+  };
+
+  // 対象のTodoを削除
+  const deleteTodo = (id: number) => {
+    setTodoList((prevTodoList) => {
+      // 対象のidでないTodoを残す
+      return prevTodoList.filter((todo) => {
+        return todo.id !== id;
+      });
+    });
+  };
+
+  // 完了したTodoをすべて削除
+  const deleteAllCompleted = () => {
+    setTodoList((prevTodoList) => {
+      // 完了していないTodoを残す
+      return prevTodoList.filter((todo) => {
+        return !todo.completed;
+      });
+    });
+  };
 
   // 対象のTodoの完了を変更
   const changeCompleted = (id: number) => {
@@ -29,8 +66,16 @@ function App() {
   return (
     <main className="mx-auto mt-10 max-w-xl space-y-10">
       <h1 className="text-center text-4xl">Todoアプリ</h1>
-      <div className="rounded bg-slate-200 p-5">
-        <TodoList todoList={todoList} changeCompleted={changeCompleted} />
+      <div className="space-y-5">
+        <AddTodoForm addTodo={addTodo} />{' '}
+        <div className="space-y-5 rounded bg-slate-200 p-5">
+          <TodoList
+            todoList={todoList}
+            changeCompleted={changeCompleted}
+            deleteTodo={deleteTodo}
+          />
+          <TodoSummary deleteAllCompleted={deleteAllCompleted} />
+        </div>
       </div>
     </main>
   );
